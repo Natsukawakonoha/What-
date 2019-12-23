8a78d75784cc3ea3968f7ce7e78c0a6ebefb5643
Natsukawakonoha <57523950+Natsukawakonoha@users.noreply.github.com>
2019-12-23 23:36:32 +0800

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,12 @@
+iny=[]
+i=len(y)
+t=p=sum=0
+while(p!=-1):
+   p=int(input("第%d个学生的成绩是： "% (t+1))
+   iny.insert(t,p)     
+   sum+=p
+   t+=1
+print("the total is %d"%sum)
+print("the average is %d"%(sum+1)/(t-1))
+
+
